6b413aaf51c4fa060c9a31527b7a5e853b44ddf9
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-05-17 11:08:50 -0700

    chore: remove msw deps and config

--- src/callFetch.spec.ts
+++ src/callFetch.spec.ts
@@ -1,18 +1,5 @@
 import { callFetch } from "./callFetch";
-import { worker } from "./__mocks__/worker";
 
 describe(callFetch.name, () => {
-  beforeAll(async () => {
-    await worker.start();
-  });
-
-  afterEach(() => {
-    worker.resetHandlers();
-  });
-
-  afterAll(() => {
-    worker.stop();
-  });
-
   it("returns ReadableStream<Uint8Array> if defined", async () => {});
 });
