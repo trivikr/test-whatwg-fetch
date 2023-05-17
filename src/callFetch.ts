ada5e581483927b0127dbefd4850eeb4ddf6130d
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-05-17 08:06:14 -0700

    chore: add example code for callFetch

---
+++ src/callFetch.ts
@@ -0,0 +1,8 @@
+export const callFetch = async (url: string) => {
+  const response = await fetch(url);
+  if (response.body != undefined) {
+    const body = await response.blob();
+    return body;
+  }
+  return response.body;
+};
