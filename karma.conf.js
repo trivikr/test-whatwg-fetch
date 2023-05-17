c9dceb51d12e4f582a2a51e6a77528bac120e330
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-05-17 08:51:58 -0700

    test: run in ChromeHeadless mode

--- karma.conf.js
+++ karma.conf.js
@@ -1,9 +1,11 @@
+process.env.CHROME_BIN = require("puppeteer").executablePath();
+
 module.exports = function (config) {
   config.set({
     frameworks: ["jasmine", "karma-typescript"],
     files: ["src/**/*.ts"],
     preprocessors: { "**/*.ts": "karma-typescript" },
     reporters: ["progress", "karma-typescript"],
-    browsers: ["Chrome"],
+    browsers: ["ChromeHeadless"],
   });
 };
