7b67363b58488af65110e9919b14e34b022fd90a
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-05-18 21:00:14 -0700

    chore: move mockServiceWorker.js to src/__mocks__

--- karma.conf.js
+++ karma.conf.js
@@ -27,7 +27,7 @@ module.exports = function (config) {
 
     // MSW must be referenced here in order for it to work
     proxies: {
-      "/mockServiceWorker.js": "/base/mockServiceWorker.js",
+      "/mockServiceWorker.js": "/base/src/__mocks__/mockServiceWorker.js",
     },
 
     singleRun: true,
